bb93c81d8295e7ace4a92356acf732c6493a0d7b
Pao Carvajal <nailil811@gmail.com>
2026-05-16 20:20:21 -0600

    fix: add UTF-8 BOM to Odoo CSV exports to prevent encoding errors

--- src/views/OdooExport.jsx
+++ src/views/OdooExport.jsx
@@ -40,7 +40,7 @@ export default function OdooExport() {
 
   const downloadCSV = (data, filename) => {
     const csv = Papa.unparse(data);
-    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    const blob = new Blob([`\uFEFF${csv}`], { type: 'text/csv;charset=utf-8;' });
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
     link.setAttribute('download', filename);
